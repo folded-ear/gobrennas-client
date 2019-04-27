989f8f9b0912036adebdca6c05c3d6ba8ba43c69
switzerb <brenna.switzer@gmail.com>
2019-04-27 11:58:13 -0700

    Add a quick way to look at the whole recipe if necessary

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import axios from 'axios';
 
 function getRecipes() {
@@ -10,32 +10,64 @@ class Recipes extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      recipes: []
+      recipes: [],
+      selected: 0
     }
   }
   
   componentDidMount() {
     getRecipes().then(result => {
       console.log(result.data);
-      this.setState({ recipes: result.data });
+      this.setState({recipes: result.data});
     });
   }
   
+  selectRecipe(id) {
+    this.setState({selected: id});
+  }
+  
   renderRecipes() {
-    const { recipes } = this.state;
+    const {recipes} = this.state;
     
-    return recipes.map( recipe => {
+    return recipes.map(recipe => {
       return (
-        <p key={recipe.title}>{recipe.title}</p>
+        <div
+          key={recipe.title}
+          onClick={() => this.selectRecipe(recipe.id)}
+        >
+          {recipe.title}
+        </div>
       )
     });
   }
   
+  renderRecipe() {
+    const {selected} = this.state;
+    const recipe = this.state.recipes.find(recipe => {
+      return recipe.id === selected;
+    });
+    return recipe ?
+      <div>
+        <h2>{recipe.title}</h2>
+        <p onClick={() => this.selectRecipe(0)}>Close Recipe</p>
+        <p>{recipe.external_url}</p>
+        <p>{recipe.ingredients}</p>
+        <p>{recipe.directions}</p>
+      </div> :
+      null;
+  }
+  
   render() {
+    const {selected} = this.state;
+    
     return (
       <div>
         <h1>CookBook</h1>
-        { this.renderRecipes()}
+        {selected ?
+          this.renderRecipe()
+          :
+          this.renderRecipes()
+        }
       </div>
     )
   }
