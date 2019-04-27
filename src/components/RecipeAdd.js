86fac8c9acbe007a278062ba55da8ea16747c957
switzerb <brenna.switzer@gmail.com>
2019-04-27 13:04:15 -0700

    Add a basic form to be able to input new recipes

--- src/components/RecipeAdd.js
+++ src/components/RecipeAdd.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import {Form, Input, Button} from 'antd';
+
+const {TextArea} = Input;
 
 const RecipeAdd = () => (
   <div>
     <h2>Add New Recipe</h2>
+    <div>
+      <Form layout="vertical">
+        <Form.Item>
+          <Input placeholder="Recipe Title"/>
+        </Form.Item>
+        <Form.Item>
+          <Input placeholder="External URL"/>
+        </Form.Item>
+        <Form.Item>
+          <TextArea placeholder="Recipe Ingredients" rows={4}/>
+        </Form.Item>
+        <Form.Item>
+          <TextArea placeholder="Recipe Directions" rows={4}/>
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary">Save</Button>
+        </Form.Item>
+      </Form>
+    </div>
   </div>
 );
 
