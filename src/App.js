b9a212c6c56fe252bec2cf348010198cd5a15ecc
switzerb <brenna.switzer@gmail.com>
2019-04-27 12:44:32 -0700

    Add react-router for navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import Recipes from './components/Recipes';
+import RecipeAdd from './components/RecipeAdd';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Recipes/>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/">Recipes</Link>
+            </li>
+            <li>
+              <Link to="/add">Add New Recipe</Link>
+            </li>
+          </ul>
+      
+          <hr />
+      
+          <Route exact path="/" component={Recipes} />
+          <Route path="/add" component={RecipeAdd} />
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/components/RecipeAdd.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const RecipeAdd = () => (
+  <div>
+    <h2>Add New Recipe</h2>
+  </div>
+);
+
+export default RecipeAdd;
