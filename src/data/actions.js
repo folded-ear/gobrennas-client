528a37554f61ac9ebc11e9175b554023ddfadf5d
switzerb <brenna.switzer@gmail.com>
2019-04-28 12:12:31 -0700

    Add new recipe by posting to API

--- src/data/actions.js
+++ src/data/actions.js
@@ -1,12 +1,29 @@
+// import { toJSON } from 'immutable';
 import Types from './types';
 import Dispatcher from './dispatcher';
 import axios from 'axios';
+import Recipe from "../models/Recipe";
 
 const Actions = {
   addRecipe(recipe) {
-    Dispatcher.dispatch({
-      type: Types.ADD_RECIPE,
-      data: recipe,
+    axios
+      .post('/api/recipe', recipe.toJSON())
+      .then((response) => {
+      //TODO: handle response back from API if there are errors, etc
+      if (response.status && response.status === 201) {
+        const {data: recipe} = response;
+
+        Dispatcher.dispatch({
+          type: Types.ADD_RECIPE,
+          data: new Recipe({
+            id: recipe.id,
+            title: recipe.title,
+            external_url: recipe.external_url,
+            ingredients: recipe.ingredients,
+            directions: recipe.directions
+          }),
+        });
+      }
     });
   },
   
