0c9f62e0434febbb9121335f6c23db8957a3b069
switzerb <brenna.switzer@gmail.com>
2019-04-28 13:08:02 -0700

    Add a rough detail view for each recipe

--- src/views/RecipesList.js
+++ src/views/RecipesList.js
@@ -5,8 +5,33 @@ const handleSelect = (id) => {
   Actions.selectRecipe(id);
 };
 
+
 function RecipesList(props) {
-  console.log(props);
+  const selected = props.recipes.get('selected');
+  const library = props.recipes.get('library');
+  
+  if(selected) {
+    
+    const recipe = library.find( recipe => {
+      return recipe.get('id') === selected;
+    });
+    
+    if(recipe) {
+      return (
+        <div>
+          <h3>{recipe.get('title')}</h3>
+          <p>{recipe.get('external_url')}</p>
+          <p>{recipe.get('ingredients')}</p>
+          <p>{recipe.get('directions')}</p>
+        </div>
+      )
+    }
+    
+    return (
+      <div>Oops.</div>
+    )
+  }
+  
   return (
     <div className="recipes-list">
       {[...props.recipes.get('library').values()].reverse().map(recipe => (
