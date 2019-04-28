0ed6d39af0311be7d4e28cb13805bab3da86af79
switzerb <brenna.switzer@gmail.com>
2019-04-28 15:00:01 -0700

    Betterment of the look and feel, for now

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
-import {Menu} from 'antd';
+import {Layout, Menu} from 'antd';
 import Recipes from './containers/Recipes';
 import RecipeAdd from './views/RecipeAdd';
 
@@ -10,32 +10,41 @@ import './App.scss';
 
 function App() {
   
+  const {Header, Content} = Layout;
   const [current, setCurrent] = useState('recipes');
   
-  useEffect( () => {
+  useEffect(() => {
     Actions.fetchRecipes();
   });
   
   return (
     <div className="App">
       <Router>
-        <h1>CookBook</h1>
-        <Menu
-          onClick={(e) => setCurrent(e.key)}
-          selectedKeys={[current]}
-          mode="horizontal">
-          <Menu.Item key="recipes">
-            <Link to="/">Recipes</Link>
-          </Menu.Item>
-          <Menu.Item key="add">
-            <Link to="/add">Add New Recipe</Link>
-          </Menu.Item>
-        </Menu>
-        
-        <div>
-          <Route exact path="/" component={Recipes}/>
-          <Route path="/add" component={RecipeAdd}/>
-        </div>
+        <Layout className="layout">
+          <Header>
+            <div className="logo">Cookbook</div>
+            <Menu
+              theme="dark"
+              style={{ lineHeight: '64px' }}
+              onClick={(e) => setCurrent(e.key)}
+              selectedKeys={[current]}
+              mode="horizontal">
+              <Menu.Item key="recipes">
+                <Link to="/">Recipes</Link>
+              </Menu.Item>
+              <Menu.Item key="add">
+                <Link to="/add">Add New Recipe</Link>
+              </Menu.Item>
+            </Menu>
+          </Header>
+          
+          <Content>
+            <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+              <Route exact path="/" component={Recipes}/>
+              <Route path="/add" component={RecipeAdd}/>
+            </div>
+          </Content>
+        </Layout>
       </Router>
     </div>
   );
