3ffa876cf2916f0e3bc226dac3fc757fc54c80d2
switzerb <brenna.switzer@gmail.com>
2019-05-21 20:40:35 -0700

    Adds todo list for the next iteration for the client

--- src/views/IngredientAdd.js
+++ src/views/IngredientAdd.js
@@ -1,41 +1,68 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {AutoComplete, Form, Input, Row, Col} from "antd";
+import * as PropTypes from "prop-types";
 
-const ingredientOptions = [
-  'Milk',
-  "Eggs",
-  "Stuff"
-];
+class IngredientAdd extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      quantity: '',
+      preparation: ''
+    }
+  }
+  
+  handleUpdate = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name] : value})
+  };
+  
+  render() {
+    
+    // TODO: Add new ingredient from here if you don't see it in the dropdown -- need UX thoughts on this
+    // TODO: Send ingredient to the parent component?
+    const {onSelect} = this.props;
+    const { quantity, preparation } = this.state;
+  
+    const ingredientOptions = [
+      'Milk',
+      "Eggs",
+      "Stuff"
+    ];
+    
+    return (
+      <Form.Item>
+        <Row>
+          <Col span={4}>
+            <Input
+              name="quantity"
+              placeholder="Quantity"
+              onChange={this.handleUpdate}
+              value={quantity}
+            />
+          </Col>
+          <Col span={8}>
+            <AutoComplete
+              name="ingredient"
+              dataSource={ingredientOptions}
+              onSelect={onSelect}
+              placeholder="Ingredients"
+            />
+          </Col>
+          <Col span={12}>
+            <Input
+              name="preparation"
+              placeholder="Preparation"
+              onChange={this.handleUpdate}
+              value={preparation}
+            />
+          </Col>
+        </Row>
+      </Form.Item>
+    )
+  }
+}
 
-const IngredientAdd = ({onSelect}) => {
-  return (
-    <Form.Item>
-      <Row>
-        <Col span={4}>
-          <Input
-            name="quantity"
-            placeholder="Quantity"
-            value={null}
-          />
-        </Col>
-        <Col span={8}>
-          <AutoComplete
-          name="ingredient"
-          dataSource={ingredientOptions}
-          onSelect={onSelect}
-          placeholder="Ingredients"
-        />
-        </Col>
-        <Col span={12}>
-          <Input
-            name="preparation"
-            placeholder="Preparation"
-            value={null}
-          />
-        </Col>
-      </Row>
-    </Form.Item>
-  )
-};
+IngredientAdd.propTypes = {onSelect: PropTypes.func};
 
 export default IngredientAdd;
