5050445902b192ab59565168c5ed6e63c20ec5eb
Barney Boisvert <bboisvert@gmail.com>
2019-06-11 09:14:17 -0700

    use port :8080 for the server in dev to reuse spring-social's GOOG keys

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 export const APP_BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'http://cook.brennaswitzer.com';
-export const API_BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : 'http://cook.brennaswitzer.com';
+export const API_BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'http://cook.brennaswitzer.com';
 export const ACCESS_TOKEN = 'accessToken';
 
 export const OAUTH2_REDIRECT_URI = APP_BASE_URL + '/oauth2/redirect';
