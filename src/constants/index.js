c3a10f4fab1b55a659073a286bd596da35d6a56f
switzerb <brenna.switzer@gmail.com>
2019-06-10 17:11:55 -0700

    Changes constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,6 @@
-export const API_BASE_URL = 'http://cook.brennaswitzer.com';
+export const APP_BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'http://cook.brennaswitzer.com';
+export const API_BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : 'http://cook.brennaswitzer.com';
 export const ACCESS_TOKEN = 'accessToken';
 
-export const OAUTH2_REDIRECT_URI = API_BASE_URL + '/oauth2/redirect';
+export const OAUTH2_REDIRECT_URI = APP_BASE_URL + '/oauth2/redirect';
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
