c559b3aec5bbabc6af3d8e83cdff9265940593c0
Barney Boisvert <bboisvert@gmail.com>
2019-06-19 21:17:12 -0700

    wrap the dummy friends API in a Store

---
+++ src/data/FriendActions.js
@@ -0,0 +1,7 @@
+const FriendActions = {
+    LOAD_FRIEND_LIST: "friend/load-friend-list",
+    FRIEND_LIST_LOADED: "friend/friend-list-loaded",
+    FRIEND_LIST_LOAD_ERROR: "friend/friend-list-load-error",
+};
+
+export default FriendActions;
---
+++ src/data/FriendStore.js
@@ -0,0 +1,57 @@
+import { ReduceStore } from "flux/utils";
+import Dispatcher from "./dispatcher";
+import LoadObject from "../util/LoadObject";
+import hotLoadObject from "../util/hotLoadObject";
+import FriendActions from "./FriendActions";
+import BaseAxios from "axios";
+import { API_BASE_URL } from "../constants/index";
+import promiseFlux from "../util/promiseFlux";
+
+const axios = BaseAxios.create({
+    baseURL: `${API_BASE_URL}/api/friends`,
+});
+
+class FriendStore extends ReduceStore {
+    constructor() {
+        super(Dispatcher);
+    }
+
+    getInitialState() {
+        return LoadObject.empty();
+    }
+
+    reduce(state, action) {
+        switch (action.type) {
+
+            case FriendActions.LOAD_FRIEND_LIST: {
+                promiseFlux(
+                    axios.get(``),
+                    data => ({
+                        type: FriendActions.FRIEND_LIST_LOADED,
+                        data: data.data,
+                    }),
+                );
+                return state.loading();
+            }
+
+            case FriendActions.FRIEND_LIST_LOADED: {
+                return state.setValue(action.data);
+            }
+
+            default:
+                return state;
+        }
+    }
+
+    getFriendsLO() {
+        return hotLoadObject(
+            () => this.getState(),
+            () => Dispatcher.dispatch({
+                type: FriendActions.LOAD_FRIEND_LIST,
+            }),
+        );
+    }
+
+}
+
+export default new FriendStore();
