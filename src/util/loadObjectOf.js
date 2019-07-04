bb463ecc4fc700c804e0e236f57c7e9cb3fee318
Barney Boisvert <bboisvert@gmail.com>
2019-07-03 17:00:26 -0700

    LoadObject typechecker

---
+++ src/util/loadObjectOf.js
@@ -0,0 +1,49 @@
+import RPTSecret from "prop-types/lib/ReactPropTypesSecret";
+import {
+    createChainableTypeChecker,
+    getClassName,
+    PropTypeError,
+} from "./typeHelpers";
+import LoadObject from "./LoadObject";
+
+const loadObjectOf = (valueTypeChecker, optionalErrorTypeChecker) =>
+    createChainableTypeChecker((
+        props,
+        propName,
+        componentName,
+        location,
+        propFullName,
+    ) => {
+        if (typeof valueTypeChecker !== 'function' || (optionalErrorTypeChecker != null && typeof optionalErrorTypeChecker !== 'function')) {
+            return new PropTypeError('Property `' + propFullName + '` of component `' + componentName + '` has invalid PropType notation inside loadObjectOf.');
+        }
+        const lo = props[propName];
+        if (!(lo instanceof LoadObject)) {
+            return new PropTypeError('Invalid ' + location + ' `' + propFullName + '` of type `' + getClassName(lo) + '` supplied to `' + componentName + '`, expected instance of `LoadObject`.');
+        }
+        if (lo.hasValue()) {
+            const error = valueTypeChecker(
+                {value: lo.getValueEnforcing()},
+                "value",
+                "LoadObject",
+                location,
+                propFullName + ".value",
+                RPTSecret,
+            );
+            if (error != null) return error;
+        }
+        if (optionalErrorTypeChecker != null && lo.hasError()) {
+            const error = valueTypeChecker(
+                {error: lo.getErrorEnforcing()},
+                "error",
+                "LoadObject",
+                location,
+                propFullName + ".error",
+                RPTSecret,
+            );
+            if (error != null) return error;
+        }
+        return null;
+    });
+
+export default loadObjectOf;
