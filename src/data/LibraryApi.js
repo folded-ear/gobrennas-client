1c1ec9065765d7a631439233d754042ebbd60965
switzerb <brenna.switzer@gmail.com>
2019-07-04 12:48:19 -0700

    Creates loading state for library

---
+++ src/data/LibraryApi.js
@@ -0,0 +1,24 @@
+import BaseAxios from "axios";
+import {API_BASE_URL} from "../constants";
+import {LibraryActions} from "./LibraryActions";
+import promiseFlux from "../util/promiseFlux";
+
+const axios = BaseAxios.create({
+    baseURL: `${API_BASE_URL}/api/recipe`,
+});
+
+const LibraryApi = {
+    
+    loadLibrary: () => {
+        promiseFlux(
+            axios.get(`/all`),
+            data => ({
+                type: LibraryActions.LIBRARY_LOADED,
+                data: data.data,
+            }),
+        );
+    }
+    
+};
+
+export default LibraryApi
--- src/data/LibraryStore.js
+++ src/data/LibraryStore.js
@@ -1,7 +1,14 @@
-import { ReduceStore } from 'flux/utils'
+import {ReduceStore} from 'flux/utils'
 import Dispatcher from './dispatcher'
-import {OrderedMap} from "immutable";
+import {
+    List,
+    OrderedMap
+} from "immutable";
 import LoadObject from "../util/LoadObject";
+import Recipe from "../models/Recipe";
+import IngredientRef from "../models/IngredientRef";
+import Ingredient from "../models/Ingredient";
+import LibraryApi from "./LibraryApi";
 
 class LibraryStore extends ReduceStore {
     
@@ -18,9 +25,48 @@ class LibraryStore extends ReduceStore {
     reduce(state, action) {
         
         switch(action.type) {
+    
+            case LibraryActions.LOAD_LIBRARY: {
+                return this.loadLibrary(state)
+            }
+                
+            case LibraryActions.LIBRARY_LOADED: {
+                let recipes = List(action.data.map(recipe => {
+                    return (new Recipe({
+                        id: recipe.ingredientId,
+                        title: recipe.title,
+                        external_url: recipe.external_url,
+                        ingredients: recipe.ingredients.map(ingredient => {
+                            return new IngredientRef({
+                                id: ingredient.ingredientId,
+                                quantity: ingredient.quantity,
+                                preparation: ingredient.preparation,
+                                ingredient: new Ingredient({
+                                    name: ingredient.ingredient.name
+                                })
+                            })
+                        }),
+                        rawIngredients: recipe.rawIngredients,
+                        directions: recipe.directions
+                    }))
+                }));
+        
+                return state.setIn(['library'], recipes);
+            }
+    
             default: {
                 return state
             }
         }
+    }
+    
+    loadLibrary(state) {
+        const lo = state.get('recipes');
+        
+        if (lo.isLoading()) {
+            return state
+        }
+        LibraryApi.loadLibrary();
+        return state.set('recipes', lo.loading());
     }
 }
