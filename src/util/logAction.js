b15eef7448706b9ee1a83d6bc57e2915947c7a26
Barney Boisvert <bboisvert@gmail.com>
2019-07-04 12:32:20 -0700

    account for typed actions when logging

--- src/util/logAction.js
+++ src/util/logAction.js
@@ -2,7 +2,9 @@ const logAction = action => {
     const temp = {...action};
     delete temp.type;
     const keys = Object.keys(temp);
-    const args = ["FLUX>", action.type];
+    const args = ["FLUX>", typeof action.type === "string"
+        ? action.type
+        : action.type.toString()];
     if (keys.length === 1) {
         args.push(keys[0], action[keys[0]])
     } else {
