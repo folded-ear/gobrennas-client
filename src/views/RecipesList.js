8848baa2f67818ebede96573c7560029efe6072b
switzerb <brenna.switzer@gmail.com>
2019-07-04 22:54:44 -0700

    Change recipelist to a functional component

--- src/views/RecipesList.js
+++ src/views/RecipesList.js
@@ -1,29 +1,25 @@
-import React, {PureComponent} from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom';
 import {Spin} from "antd";
 
-
-class RecipesList extends PureComponent<{}> {
+const RecipesList = (props: {}) => {
+    const {libraryLO} = props;
     
-    render() {
-        const {libraryLO} = this.props;
-        
-        if (!libraryLO.hasValue()) {
-            return <Spin tip="Loading recipe library..."/>
-        }
-        
-        const library = libraryLO.getValueEnforcing();
-        return (
-            <div className="recipes-list">
-                <h1>Recipes</h1>
-                {[...library].reverse().map(recipe => (
-                    <Link to={`/recipe/${recipe.ingredientId}`} key={recipe.ingredientId}>
-                        {recipe.title}
-                    </Link>
-                ))}
-            </div>
-        );
+    if (!libraryLO.hasValue()) {
+        return <Spin tip="Loading recipe library..."/>
     }
+    
+    const library = libraryLO.getValueEnforcing();
+    return (
+        <div className="recipes-list">
+            <h1>Recipes</h1>
+            {[...library].reverse().map(recipe => (
+                <Link to={`/recipe/${recipe.ingredientId}`} key={recipe.ingredientId}>
+                    {recipe.title}
+                </Link>
+            ))}
+        </div>
+    );
 }
 
 export default RecipesList;
