98055acebb090540990aaec14b0dd17cf1f9747d
Barney Boisvert <bboisvert@gmail.com>
2019-11-24 08:10:18 -0800

    reflow recipe navigation

--- src/views/RecipeAdd.js
+++ src/views/RecipeAdd.js
@@ -2,17 +2,32 @@ import React, { Component } from "react"
 import RecipeForm from "../containers/RecipeForm"
 import Dispatcher from "../data/dispatcher"
 import RecipeActions from "../data/RecipeActions"
+import history from "../util/history"
+import onNextActionThat from "../util/onNextActionThat"
+import DraftRecipeStore from "../data/DraftRecipeStore"
 
 const handleSave = (recipe) => {
     Dispatcher.dispatch({
         type: RecipeActions.CREATE_RECIPE,
         data: recipe
     })
+    onNextActionThat(action =>
+        action.type === RecipeActions.RECIPE_CREATED,
+        (action) => {
+            if (action.id !== recipe.id) return
+            history.push(`/library/recipe/${action.data.id}`)
+        })
 }
 
-const handleCancel = () => Dispatcher.dispatch({
-    type: RecipeActions.CANCEL_ADD,
-})
+const handleCancel = () => {
+    const draft = DraftRecipeStore.getDraft()
+    Dispatcher.dispatch({
+        type: RecipeActions.CANCEL_ADD,
+    })
+    history.push(draft.sourceId == null
+        ? "/library"
+        : `/library/recipe/${draft.sourceId}`)
+}
 
 class RecipeAdd extends Component<{}> {
     render() {
