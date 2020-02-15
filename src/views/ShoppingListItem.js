17485143f4c4311befca79187787f739b3878350
Barney Boisvert <bboisvert@gmail.com>
2020-02-15 10:07:31 -0800

    Add space after "and" between non-agg quantities

--- src/views/ShoppingListItem.js
+++ src/views/ShoppingListItem.js
@@ -9,7 +9,7 @@ const ShoppingListItem = it =>
             {it.ingredient.name} ({it.quantities.map((q, i, a) =>
             <span key={q.units}>
                 {i > 0 && ", "}
-                {i > 0 && i === a.length - 1 && " and"}
+                {i > 0 && i === a.length - 1 && " and "}
                 <Quantity
                     quantity={q.quantity}
                     units={q.units}/>
