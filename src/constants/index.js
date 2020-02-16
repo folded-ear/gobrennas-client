bdfb3950b3fe169fd27d69c289d8bb1cdab614f2
Barney Boisvert <bboisvert@gmail.com>
2020-02-16 15:00:30 -0800

    prod is HTTPS, dev is HTTP (neither are scheme-relative)

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
-export const APP_BASE_URL = process.env.NODE_ENV === 'development' ? '//localhost:3001' : '//cook.brennaswitzer.com'
-export const API_BASE_URL = process.env.NODE_ENV === 'development' ? '//localhost:8080' : '//cook.brennaswitzer.com'
+export const APP_BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://cook.brennaswitzer.com'
+export const API_BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'https://cook.brennaswitzer.com'
 export const LOCAL_STORAGE_ACCESS_TOKEN = 'accessToken'
 export const LOCAL_STORAGE_PREFERENCES = "preferences"
 export const SESSION_STORAGE_POST_LOGIN = "postLogin"
