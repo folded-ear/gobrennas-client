e38abfca81ab3ab88252eb2f4596d8fe3e2206eb
Brenna Switzer <brenna.switzer@gmail.com>
2020-08-23 09:38:48 -0700

    chore: add responsiveness

--- src/views/common/AppHeader.js
+++ src/views/common/AppHeader.js
@@ -1,11 +1,12 @@
-import React from "react";
-import {Link, withRouter} from "react-router-dom";
+import React from "react"
+import {Link, withRouter} from "react-router-dom"
 import {
     AppBar,
     Box,
     IconButton,
     Tab,
-    Toolbar
+    Toolbar,
+    useMediaQuery
 } from "@material-ui/core"
 import {
     AccountCircle,
@@ -13,10 +14,12 @@ import {
     ExitToApp,
     MenuBook,
     PostAdd
-} from "@material-ui/icons";
-import {makeStyles} from "@material-ui/core/styles"
-import clsx from "clsx";
-import Logo from "./Logo";
+} from "@material-ui/icons"
+import {
+    makeStyles,
+    useTheme
+} from "@material-ui/core/styles"
+import Logo from "./Logo"
 
 const styles = makeStyles(theme => ({
     root: {
@@ -25,10 +28,35 @@ const styles = makeStyles(theme => ({
     bar: {
         flexGrow: 1
     }
-}));
+}))
 
 const AppHeader = ({authenticated, onLogout}) => {
-    const classes = styles();
+    const classes = styles()
+    const theme = useTheme()
+    const mobile = useMediaQuery(theme.breakpoints.down('sm'))
+
+    const renderMobile = () => {
+        return (
+            <Box className={classes.bar}>
+                <IconButton component={Link} to="library" value="library"><MenuBook/></IconButton>
+                <IconButton component={Link} to="add" value="add recipe"><PostAdd/></IconButton>
+                <IconButton component={Link} to="tasks" value="tasks"><EventNote/></IconButton>
+            </Box>
+        )
+    }
+
+    const renderDesktop = () => {
+        return (
+            <>
+                <Logo/>
+                <Box className={classes.bar}>
+                    <Tab icon={<MenuBook/>} label="Library" component={Link} to="library"/>
+                    <Tab icon={<PostAdd/>} label="New" component={Link} to="add"/>
+                    <Tab icon={<EventNote/>} label="Tasks" component={Link} to="tasks"/>
+                </Box>
+            </>
+        )
+    }
 
     return (
         <>
@@ -40,14 +68,19 @@ const AppHeader = ({authenticated, onLogout}) => {
                     {
                         authenticated ?
                             <Toolbar>
-                                <Logo />
-                                <Box className={classes.bar}>
-                                <Tab icon={<MenuBook />} label="Library" component={Link} to="library" />
-                                <Tab icon={<PostAdd />} label="Add Recipe" component={Link} to="add" />
-                                <Tab icon={<EventNote />} label="Tasks" component={Link} to="tasks" />
-                                </Box>
-                                <IconButton component={Link} to="profile" value="profile" title="Profile"><AccountCircle /></IconButton>
-                                <IconButton onClick={onLogout} title="Logout"><ExitToApp /></IconButton>
+                                {mobile ? renderMobile() : renderDesktop()}
+                                <IconButton
+                                    component={Link}
+                                    to="profile"
+                                    value="profile"
+                                    title="Profile">
+                                    <AccountCircle/>
+                                </IconButton>
+                                <IconButton
+                                    onClick={onLogout}
+                                    title="Logout">
+                                    <ExitToApp/>
+                                </IconButton>
                             </Toolbar>
                             :
                             <div>&nbsp;</div>
