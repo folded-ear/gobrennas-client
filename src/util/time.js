7b979f779a4e8784feccee6b039334a2e5819c91
Brenna Switzer <brenna.switzer@gmail.com>
2020-09-07 20:29:45 -0700

    Return null if no time

--- src/util/time.js
+++ src/util/time.js
@@ -1,7 +1,9 @@
 export const toMilliseconds = (time) => {
+    if(!time) { return null }
     return time * 60 * 1000;
 }
 
 export const fromMilliseconds = (time) => {
+    if(!time) { return null }
     return time / (60 * 1000);
 }
