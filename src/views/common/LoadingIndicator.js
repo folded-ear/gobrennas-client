514a4e9393ce6c359c9b005d3c3b6945c7c7973e
Barney Boisvert <bboisvert@gmail.com>
2020-09-12 11:10:20 -0700

    use Paper for loading indicator, w/ custom content support

--- src/views/common/LoadingIndicator.js
+++ src/views/common/LoadingIndicator.js
@@ -1,9 +1,30 @@
+import { Typography } from "@material-ui/core";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Paper from "@material-ui/core/Paper";
+import PropTypes from "prop-types";
 import React from "react";
 
-export default function LoadingIndicator() {
-    return (
-        <div className="loading-indicator" style={{display: "block", textAlign: "center", marginTop: "30px"}}>
-            Loading ...
-        </div>
-    );
+export default function LoadingIndicator({
+    primary = "Loading ...",
+    children,
+}) {
+    return <Paper
+        style={{
+            textAlign: "center",
+            paddingTop: "2em",
+            paddingBottom: "1em",
+        }}
+    >
+        <CircularProgress />
+        {children
+            ? children
+            : primary && <Typography component="p">
+                {primary}
+            </Typography>}
+    </Paper>;
 }
+
+LoadingIndicator.propTypes = {
+    primary: PropTypes.string,
+    children: PropTypes.node,
+};
