29243e17cc92fe13c2648b32442d257dd0dfbed8
Barney Boisvert <bboisvert@gmail.com>
2020-10-10 22:09:28 -0700

    remove the passage of time as an agent acting on the app

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { Router } from "react-router-dom";
 import App from "./App";
 import Dispatcher from "./data/dispatcher";
-import TemporalActions from "./data/TemporalActions";
 import WindowActions from "./data/WindowActions";
 import * as serviceWorker from "./serviceWorker";
 import debounce from "./util/debounce";
@@ -51,40 +50,6 @@ document.addEventListener("visibilitychange", () =>
         type: WindowActions.VISIBILITY_CHANGE,
         visible: !document.hidden,
     }));
-
-{
-    let last = new Date();
-    setInterval(() => {
-        const now = new Date();
-        Dispatcher.dispatch({type: TemporalActions.EVERY_15_SECONDS});
-        if (last.getMinutes() !== now.getMinutes()) {
-            Dispatcher.dispatch({type: TemporalActions.EVERY_MINUTE});
-        }
-        if (last.getHours() !== now.getHours()) {
-            Dispatcher.dispatch({type: TemporalActions.EVERY_HOUR});
-            if (last.getHours() < 6 && now.getHours() >= 6) {
-                // i have decreed that the morning starts at 6am!
-                Dispatcher.dispatch({type: TemporalActions.EVERY_MORNING});
-            }
-            if (last.getHours() < 12 && now.getHours() >= 12) {
-                // it's pretty well accepted that afternoon starts at noon
-                Dispatcher.dispatch({type: TemporalActions.EVERY_AFTERNOON});
-            }
-            if (last.getHours() < 17 && now.getHours() >= 17) {
-                // i have decreed that the evening starts at 5pm!
-                Dispatcher.dispatch({type: TemporalActions.EVERY_EVENING});
-            }
-            if (last.getHours() < 21 && now.getHours() >= 21) {
-                // i have decreed that night starts at 9pm!
-                Dispatcher.dispatch({type: TemporalActions.EVERY_NIGHT});
-            }
-        }
-        if (last.getDate() !== now.getDate()) {
-            Dispatcher.dispatch({type: TemporalActions.EVERY_DAY});
-        }
-        last = now;
-    }, 1000 * 15);
-}
 
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register({
