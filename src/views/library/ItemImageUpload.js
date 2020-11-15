32e9767d96596d497ca7a8ea9c1e9033d6e0c506
Barney Boisvert <bboisvert@gmail.com>
2020-11-14 17:37:53 -0800

    allow click-to-upload for assigning recipe photos too.

--- src/views/library/ItemImageUpload.js
+++ src/views/library/ItemImageUpload.js
@@ -7,12 +7,26 @@ import { Recipe } from "../../data/RecipeTypes";
 
 const useStyles = makeStyles({
     root: {
+        display: "block",
         height: 140,
         textAlign: "center",
         paddingTop: "60px",
         backgroundColor: "#eee"
     },
 });
+
+const sendOffFirstFile = (files, recipe) => {
+    for (let i = 0; i < files.length; i++) {
+        const f = files[i];
+        if (f.type.indexOf("image/") !== 0) continue;
+        Dispatcher.dispatch({
+            type: RecipeActions.SET_RECIPE_PHOTO,
+            id: recipe.id,
+            photo: f,
+        });
+        break;
+    }
+};
 
 const ItemImageUpload = ({recipe}) => {
     const classes = useStyles();
@@ -26,27 +40,35 @@ const ItemImageUpload = ({recipe}) => {
         event.preventDefault();
         const dt = event.dataTransfer;
         const files = dt.files;
-        for (let i = 0; i < files.length; i++) {
-            const f = files[i];
-            if (f.type.indexOf("image/") !== 0) continue;
-            Dispatcher.dispatch({
-                type: RecipeActions.SET_RECIPE_PHOTO,
-                id: recipe.id,
-                photo: f,
-            });
-            break;
-        }
+        sendOffFirstFile(files, recipe);
     };
 
+    const handleFileSelect = event => {
+        sendOffFirstFile(event.target.files, recipe);
+    };
+
+    const inputId = `file-uploaded-recipe-${recipe.id}`;
     return (
-        <div
+        <label
+            htmlFor={inputId}
             className={classes.root}
             onDrop={handleDrop}
             onDragOver={handleDragOver}
             title="Drag and drop a photo here to assign it to the recipe."
         >
             <AddAPhoto color="disabled" />
-        </div>);
+            <input
+                id={inputId}
+                accept="image/*"
+                type="file"
+                style={{
+                    opacity: 0,
+                    height: 0,
+                    width: 0,
+                }}
+                onChange={handleFileSelect}
+            />
+        </label>);
 };
 
 ItemImageUpload.propTypes = {
