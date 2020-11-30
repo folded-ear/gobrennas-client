ed277af16cd6651e7af9ef6e7e6df93c5106e1cc
Brenna Switzer <brenna.switzer@gmail.com>
2020-11-29 19:44:18 -0800

    LinkIcon needs to be a default export

--- src/views/IngredientItem.js
+++ src/views/IngredientItem.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {LinkIcon} from "@material-ui/icons/Link";
+import LinkIcon from "@material-ui/icons/Link";
 import {Container} from "flux/utils";
 import PropTypes from "prop-types";
 import {Link} from "react-router-dom";
