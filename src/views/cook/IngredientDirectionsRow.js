5625dfd9f7c8e3415dd0e8a3706d9c2a8f46e8ab
Barney Boisvert <bboisvert@gmail.com>
2021-02-23 17:21:48 -0800

    inline subrecipes on the recipe detail (collapsed)

---
+++ src/views/cook/IngredientDirectionsRow.js
@@ -0,0 +1,43 @@
+import {
+    Grid,
+    List,
+    ListItem,
+    Typography,
+} from "@material-ui/core";
+import PropTypes from "prop-types";
+import React from "react";
+import { Recipe } from "../../data/RecipeTypes";
+import Directions from "../common/Directions";
+import IngredientItem from "../IngredientItem";
+
+const IngredientDirectionsRow = ({recipe, loggedIn, hideHeadings}) => <>
+    <Grid item xs={12} md={5}>
+        {recipe.ingredients && recipe.ingredients.length > 0 && <>
+            {!hideHeadings &&<Typography variant="h5">Ingredients</Typography>}
+            <List>
+                {recipe.ingredients.map((it, i) =>
+                    <ListItem key={`${i}:${it.raw}`}>
+                        <IngredientItem
+                            ingredient={it}
+                            loggedIn={loggedIn}
+                        />
+                    </ListItem>)}
+            </List>
+        </>}
+    </Grid>
+
+    <Grid item xs={12} md={7}>
+        {recipe.directions && <React.Fragment>
+            {!hideHeadings && <Typography variant="h5">Directions</Typography>}
+            <Directions text={recipe.directions} />
+        </React.Fragment>}
+    </Grid>
+</>;
+
+IngredientDirectionsRow.propTypes = {
+    recipe: Recipe.isRequired,
+    loggedIn: PropTypes.bool,
+    hideHeadings: PropTypes.bool,
+};
+
+export default IngredientDirectionsRow;
