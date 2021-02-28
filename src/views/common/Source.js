569fa8bdc0c1fd79cb694c265cf9e36d833aeeab
Barney Boisvert <bboisvert@gmail.com>
2021-02-28 08:15:55 -0800

    add noopener to the external link's rel too

--- src/views/common/Source.js
+++ src/views/common/Source.js
@@ -1,10 +1,10 @@
-import React from "react";
 import PropTypes from "prop-types";
+import React from "react";
 
 const Source = ({url}) => {
     try {
         const source = new URL(url);
-        return (<a href={url} target="_blank" rel="noreferrer">{source.hostname}</a>);
+        return (<a href={url} target="_blank" rel="noreferrer noopener">{source.hostname}</a>);
     } catch (e) {
         return url;
     }
