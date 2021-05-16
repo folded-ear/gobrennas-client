4eb6725d3a919433265d7866fa657402fa05f0eb
Barney Boisvert <bboisvert@gmail.com>
2021-05-16 07:30:52 -0700

    promote the compact cards out of dev mode

--- src/views/library/RecipeCard.js
+++ src/views/library/RecipeCard.js
@@ -1,23 +1,14 @@
 import {
     Box,
-    Button,
     Card,
-    CardActions,
     CardContent,
     Typography,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import {
-    Edit,
-    MenuBook,
-} from "@material-ui/icons";
 import PropTypes from "prop-types";
 import React from "react";
 import { Link } from "react-router-dom";
-import Dispatcher from "../../data/dispatcher";
 import FriendStore from "../../data/FriendStore";
-import PreferencesStore from "../../data/PreferencesStore";
-import RecipeActions from "../../data/RecipeActions";
 import { Recipe } from "../../data/RecipeTypes";
 import useFluxStore from "../../data/useFluxStore";
 import { loadObjectOf } from "../../util/loadObjectTypes";
@@ -25,7 +16,6 @@ import { formatDuration } from "../../util/time";
 import RecipeInfo from "../common/RecipeInfo";
 import Source from "../common/Source";
 import LabelItem from "../LabelItem";
-import SendToPlan from "../SendToPlan";
 import User from "../user/User";
 import ItemImage from "./ItemImage";
 import ItemImageUpload from "./ItemImageUpload";
@@ -47,11 +37,6 @@ const RecipeCard = ({recipe, mine}) => {
             : FriendStore.getFriendLO(recipe.ownerId),
         [FriendStore],
         [mine, recipe.ownerId],
-    );
-    const devMode = useFluxStore(
-        () => PreferencesStore.isDevMode(),
-        [PreferencesStore],
-        [],
     );
     const classes = useStyles();
 
@@ -80,7 +65,7 @@ const RecipeCard = ({recipe, mine}) => {
                     />
                 }
                 <CardContent>
-                    {devMode && friendLO && friendLO.hasValue() && <div
+                    {friendLO && friendLO.hasValue() && <div
                         style={{
                             float: "right",
                         }}
@@ -111,37 +96,6 @@ const RecipeCard = ({recipe, mine}) => {
                     </Box>}
                 </CardContent>
             </div>
-            {!devMode && <CardActions>
-                <Button
-                    variant="contained"
-                    color="secondary"
-                    disableElevation
-                    startIcon={<MenuBook/>}
-                    component={Link}
-                    to={`/library/recipe/${recipe.id}`}
-                >
-                    View
-                </Button>
-                {mine && <Button
-                    variant="contained"
-                    color="secondary"
-                    disableElevation
-                    startIcon={<Edit/>}
-                    to={`/library/recipe/${recipe.id}/edit`}
-                    component={Link}
-                >
-                    Edit
-                </Button>}
-                {friendLO && friendLO.hasValue() && <User
-                    {...friendLO.getValueEnforcing()}
-                    iconOnly
-                />}
-                <SendToPlan onClick={planId => Dispatcher.dispatch({
-                    type: RecipeActions.SEND_TO_PLAN,
-                    recipeId: recipe.id,
-                    planId,
-                })}/>
-            </CardActions>}
         </Card>
     );
 };
