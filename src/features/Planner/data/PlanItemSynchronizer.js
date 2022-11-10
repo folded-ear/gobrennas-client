ad71922f9cf507b46e237df72e729e4a4c333f10
Barney Boisvert <bboisvert@gmail.com>
2022-11-10 08:10:27 -0800

    centralize synchronizer logic

--- src/features/Planner/data/PlanItemSynchronizer.js
+++ src/features/Planner/data/PlanItemSynchronizer.js
@@ -1,27 +1,24 @@
-import React from "react";
-import { useQuery } from "react-query";
 import Dispatcher from "../../../data/dispatcher";
 import PlanApi from "./PlanApi";
 import TaskActions from "./TaskActions";
+import useSynchronizer from "../../../util/useSynchronizer";
 
 function PlanItemSynchronizer({ planId }) {
-    const [ ts, setTs ] = React.useState(Date.now());
-    const {
-        data,
-    } = useQuery([ "plan", planId, "items" ], () =>
-        PlanApi.getItemsUpdatedSince(planId, ts).then(data => {
-            setTs(Date.now());
-            return data.data;
-        }));
-    React.useEffect(() => {
-        if (!data) return;
-        if (data.length === 0) return;
-        Dispatcher.dispatch({
-            type: TaskActions.LIST_DELTAS,
-            id: planId,
-            data,
-        });
-    }, [ data, planId ]);
+    useSynchronizer(
+        [ "plan", planId, "items" ],
+        ts =>
+            PlanApi.getItemsUpdatedSince(planId, ts)
+                .then(data => data.data)
+                .then(data => {
+                    if (!data) return;
+                    if (data.length === 0) return;
+                    Dispatcher.dispatch({
+                        type: TaskActions.LIST_DELTAS,
+                        id: planId,
+                        data,
+                    });
+                }),
+    );
     return null;
 }
 
