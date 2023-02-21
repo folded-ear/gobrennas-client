0271005de50c8c72299e74a9cae53cf60ff52d94
bswitzer <brenna.switzer@gmail.com>
2023-02-20 18:22:57 -0800

    add types for react-router-dom

--- src/views/common/FluxRoute.tsx
+++ src/views/common/FluxRoute.tsx
@@ -1,16 +1,24 @@
-import PropTypes from "prop-types";
-import React from "react";
-import { Route } from "react-router-dom";
+import * as React from "react";
+import {match, Route} from "react-router-dom";
 import Dispatcher from "../../data/dispatcher";
 import RouteActions from "../../data/RouteActions";
 import RouteStore from "../../data/RouteStore";
 import GTag from "../../GTag";
+import {RouteComponentProps} from "react-router";
 
-const FluxRoute = ({render, component: Component, ...rest}) => (
+type FluxRouteProps = {
+    render?: ((props: RouteComponentProps) => React.ReactNode),
+    component?: React.ElementType,
+    match?: match,
+    location?: any,
+}
+
+const FluxRoute: React.FC<FluxRouteProps> = ({render, component: Component, ...rest}) => (
     <Route
         {...rest}
         render={props => {
             const { match, location } = props;
+            // @ts-ignore
             const curr = RouteStore.getState();
             if (curr == null || match.url !== curr.url) {
                 const work = () => Dispatcher.dispatch({
@@ -28,17 +36,10 @@ const FluxRoute = ({render, component: Component, ...rest}) => (
             // this follows Route's split behaviour
             return Component != null
                 ? <Component {...props} />
-                : render(props);
+                : render ? render(props) : null;
         }
         }
     />
 );
-
-FluxRoute.propTypes = {
-    render: PropTypes.func,
-    component: PropTypes.elementType,
-    match: PropTypes.object,
-    location: PropTypes.object,
-};
 
 export default FluxRoute;
