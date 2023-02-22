b4c22d79b9789e3babefe8794469551667b48db3
bswitzer <brenna.switzer@gmail.com>
2023-02-21 20:10:48 -0800

    migrate js -> tsx

--- src/views/UserProfile/UserProfileView.tsx
+++ src/views/UserProfile/UserProfileView.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import PageBody from "../common/PageBody";
-import {UserProfileController} from "../../features/UserProfile/UserProfileController";
+import { UserProfileController } from "../../features/UserProfile/UserProfileController";
 
 // Not yet in use
 export const UserProfileView = () => {
