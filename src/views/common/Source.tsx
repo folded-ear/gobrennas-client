567a92ea5b26c7535612e5ee8b276fca595bfd8b
Barney Boisvert <bboisvert@gmail.com>
2023-03-26 19:49:50 -0700

    proptypes -> typescript

--- src/views/common/Source.tsx
+++ src/views/common/Source.tsx
@@ -1,17 +1,16 @@
-import PropTypes from "prop-types";
 import React from "react";
 
-const Source = ({url}) => {
+interface Props {
+    url: string
+}
+
+const Source: React.FC<Props> = ({ url }) => {
     try {
         const source = new URL(url);
         return (<a href={url} target="_blank" rel="noreferrer noopener">{source.hostname}</a>);
     } catch (e) {
-        return url;
+        return <>{url}</>;
     }
-};
-
-Source.propTypes = {
-    url: PropTypes.string
 };
 
 export default Source;
