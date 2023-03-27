f2a09d7249267bb5f3b237699202b07443814dc2
Barney Boisvert <bboisvert@gmail.com>
2023-03-26 19:38:43 -0700

    proptypes -> typescript

--- src/views/common/Directions.tsx
+++ src/views/common/Directions.tsx
@@ -1,8 +1,11 @@
-import PropTypes from "prop-types";
 import React from "react";
 import Markdown from "./Markdown";
 
-const Directions = ({text}) => {
+interface Props {
+    text?: string
+}
+
+const Directions: React.FC<Props> = ({ text }) => {
     if (text == null) return null;
     text = text.trim();
     if (text.length === 0) return null;
@@ -10,13 +13,12 @@ const Directions = ({text}) => {
     if (/(^|\n) *(1[.)]|[-*]) +\S.*\n/.test(text)) {
         return <Markdown text={text} />;
     }
-    // ok, just split it on line breaks and call it good.
-    return text.split("\n").map((line, i) =>
-        <p key={i}>{line}</p>);
-};
-
-Directions.propTypes = {
-    text: PropTypes.string.isRequired,
+    // ok, just split paragraphs at line breaks and call it good.
+    return <>
+        {text.split("\n")
+            .map((line, i) =>
+                <p key={i}>{line}</p>)}
+    </>;
 };
 
 export default Directions;
