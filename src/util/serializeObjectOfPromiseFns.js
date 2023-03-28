7809efd1f92d9bbc755e6fd0ce953b5a737ab6b4
Barney Boisvert <bboisvert@gmail.com>
2023-03-27 19:10:01 -0700

    serialize ElEdit recognition requests
    Cloud Run falls over when even a double handful of concurrent requests
    are made, so serialize them. It's not much slower for small recipes, and
    doesn't fail for large ones. Win!
    
    This does the same thing for the planner, though it was already mostly
    serialized but the store's internal queues.

--- src/util/serializeObjectOfPromiseFns.js
+++ src/util/serializeObjectOfPromiseFns.js
@@ -1,12 +1,11 @@
 /**
  * I wrap an object where every property is a Promise-returning function with a
  * single queue so that at most one unsettled Promise exists at a time,
- * regardless of how many time and how quickly the functions on the object are
+ * regardless of how many times or how quickly the functions on the object are
  * invoked. Invocations will be played in FIFO order, synchronously if no prior
  * invocation is in flight, asynchronously if there is.
  *
- * Note that a single queue is used for all functions. If you want per-function
- * queues, check out `serializePromiseFn`.
+ * Note that a single queue is used for all functions, not per-function queues.
  *
  * @param obj An object whose own properties are all Promise-returning functions
  * @returns a new object, with the same property keys, each of which is a new
@@ -15,24 +14,20 @@
  *  up invocations to the wrapped object's functions.
  */
 const serializeObjectOfPromiseFns = obj => {
-    const queue = []; // {fn,args}
-    let pending = false;
+    let queue = Promise.resolve();
     const result = {};
-    const flush = () => {
-        if (pending) return;
-        if (queue.length === 0) return;
-        pending = true;
-        const {fn, args} = queue.shift();
-        fn(...args).finally(() => {
-            pending = false;
-            flush();
-        });
+    const enqueue = (fn, args) => {
+        queue = queue
+            .catch(error =>
+                // eslint-disable-next-line no-console
+                console.error("Error in Promise", error))
+            .then(() =>
+                fn(...args));
+        return queue;
     };
     for (const k of Object.keys(obj)) {
-        result[k] = (...args) => {
-            queue.push({fn: obj[k], args});
-            flush();
-        };
+        result[k] = (...args) =>
+            enqueue(obj[k], args);
     }
     return result;
 };
