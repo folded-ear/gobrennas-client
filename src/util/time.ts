71a9f3a2cde1a92639e584047accbc0eef553832
Barney Boisvert <bboisvert@gmail.com>
2023-03-29 21:03:16 -0700

    js -> ts

--- src/util/time.ts
+++ src/util/time.ts
@@ -1,14 +1,16 @@
-export const toMilliseconds = (time) => {
+import { Maybe } from "graphql/jsutils/Maybe";
+
+export const toMilliseconds = (time?: number) => {
     if (!time) return null;
     return time * 60 * 1000;
 };
 
-export const fromMilliseconds = (time) => {
+export const fromMilliseconds = (time?: number) => {
     if (!time) return null;
     return time / (60 * 1000);
 };
 
-export const formatDuration = minutes => {
+export const formatDuration = (minutes: number) => {
     minutes = Math.ceil(minutes); // round up
     if (minutes < 60) {
         const unit = minutes === 1 ? "minute" : "minutes";
@@ -25,7 +27,7 @@ export const formatDuration = minutes => {
     }
 };
 
-export const formatTimer = seconds => {
+export const formatTimer = (seconds?: number) => {
     if (seconds == null || isNaN(seconds)) return "";
     const neg = seconds < 0;
     if (neg) seconds = -seconds;
@@ -33,7 +35,7 @@ export const formatTimer = seconds => {
     const min = Math.floor(seconds % 3600 / 60);
     const sec = Math.floor(seconds % 60);
     let result = hr === 0
-        ? min
+        ? "" + min
         : hr + ":" + pad(min);
     result += ":" + pad(sec);
     if (neg) result = "-" + result;
@@ -41,14 +43,15 @@ export const formatTimer = seconds => {
 };
 
 const TEN_YEARS_FROM_NOW_THIS_CENTURY = new Date().getFullYear() % 100 + 10;
-export const parseLocalDate = date => {
+
+export function parseLocalDate(date: string): Maybe<Date> {
     if (!date) return null;
     if (!/^\d+-\d+-\d+(\D|$)/.test(date)) return null;
     const parts = date.split(/\D/)
         .slice(0, 3)
-        .map(p => parseInt(p, 10));
+        .map(p => parseInt(p, 10)) as [ number, number, number ];
     if (parts.some(p => isNaN(p))) return null;
-    let year = parts.shift();
+    let year = parts.shift() as number; // parts is non-empty
     if (year < 0) return null;
     if (year <= TEN_YEARS_FROM_NOW_THIS_CENTURY) {
         year += 2000;
@@ -59,26 +62,28 @@ export const parseLocalDate = date => {
     parts.unshift(year);
     parts[1] -= 1;
     return new Date(...parts);
-};
+}
 
-const pad = number => {
+function pad(number: number): string {
     if (number < 10) {
         return "0" + number;
     }
-    return number;
-};
+    return "" + number;
+}
 
-export const formatLocalDate = date => {
+export function formatLocalDate(date?: Date): Maybe<string> {
     if (!date) return null;
     return date.getFullYear() +
         "-" + pad(date.getMonth() + 1) +
         "-" + pad(date.getDate());
-};
+}
 
 const humanDateFormatter = new Intl.DateTimeFormat("default", {
     month: "short",
     weekday: "short",
     day: "numeric",
 });
-export const humanDate = date =>
-    date ? humanDateFormatter.format(date) : "";
+
+export function humanDate(date?: Date): string {
+    return date ? humanDateFormatter.format(date) : "";
+}
