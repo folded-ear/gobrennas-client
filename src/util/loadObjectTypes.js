aab1ae7123a6ebe2668bf53a84920a3d7081eb29
Barney Boisvert <bboisvert@gmail.com>
2023-04-28 19:36:42 -0700

    push the LOs up out of TaskList

--- src/util/loadObjectTypes.js
+++ src/util/loadObjectTypes.js
@@ -8,6 +8,50 @@ import {
     PropTypeError,
 } from "./typeHelpers";
 
+export const ripLoadObject = lo => ({
+    loading: lo.isLoading(),
+    deleting: lo.isDeleting(),
+    data: lo.getValue(),
+    error: lo.getError(),
+});
+
+export const rippedLoadObjectOf = (dataTypeChecker, optionalErrorTypeChecker) =>
+    createChainableTypeChecker((
+        props,
+        propName,
+        componentName,
+        location,
+        propFullName,
+    ) => {
+        if (typeof dataTypeChecker !== "function" || (optionalErrorTypeChecker != null && typeof optionalErrorTypeChecker !== "function")) {
+            return new PropTypeError("Property `" + propFullName + "` of component `" + componentName + "` has invalid PropType notation inside rippedLoadObjectOf.");
+        }
+        const rippedLO = props[propName];
+        if (dataTypeChecker && rippedLO.data) {
+            const error = dataTypeChecker(
+                rippedLO,
+                "data",
+                "RippedLoadObject",
+                location,
+                propFullName + ".data",
+                RPTSecret,
+            );
+            if (error != null) return error;
+        }
+        if (optionalErrorTypeChecker != null && rippedLO.error) {
+            const error = optionalErrorTypeChecker(
+                rippedLO,
+                "error",
+                "RippedLoadObject",
+                location,
+                propFullName + ".error",
+                RPTSecret,
+            );
+            if (error != null) return error;
+        }
+        return null;
+    });
+
 function checkLoadObject(
     lo,
     valueTypeChecker,
