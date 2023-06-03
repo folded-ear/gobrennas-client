5b2346ceb4e183740844a7544f6c2423544d091d
Barney Boisvert <bboisvert@gmail.com>
2023-06-03 12:46:08 -0700

    indicate the active plan in the sidebar

--- src/features/Navigation/components/NavPlanItem.tsx
+++ src/features/Navigation/components/NavPlanItem.tsx
@@ -1,10 +1,14 @@
-import { TripOrigin as PlanIcon } from "@mui/icons-material";
+import {
+    RadioButtonChecked as ActivePlanIcon,
+    RadioButtonUnchecked as PlanIcon,
+} from "@mui/icons-material";
 import * as React from "react";
 import {
     ListItemButton,
     ListItemIcon,
     Typography
 } from "@mui/material";
+import useActivePlanner from "../../../data/useActivePlanner";
 
 type NavPlanItemProps = {
     id: string | number,
@@ -15,9 +19,13 @@ type NavPlanItemProps = {
 }
 
 export const NavPlanItem: React.FC<NavPlanItemProps> = ({onSelect, expanded, name, color, id}) => {
+    const active = useActivePlanner().data?.id === id;
+    const Icon = active
+        ? ActivePlanIcon
+        : PlanIcon;
     return (<ListItemButton onClick={() => onSelect(id)} key={id}>
         <ListItemIcon>
-            <PlanIcon sx={{color: color}}/>
+            <Icon sx={{ color: color }} />
         </ListItemIcon>
         <Typography noWrap>
             {expanded ? name : null}
