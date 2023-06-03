28beac755a8309010b07f6b23f5d95adaf26cfad
Barney Boisvert <bboisvert@gmail.com>
2023-06-03 15:55:45 -0700

    hash plan IDs differently for hopefully better distribution

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,28 +1,33 @@
 import {
     blue,
     blueGrey,
-    green,
     indigo,
     lightGreen,
     lime,
-    pink,
     purple,
     red,
     teal
 } from "@mui/material/colors";
 
 export const planColors = [
+    // ¡¡ Make sure this always has a size equal to a power of 2 !!
     red[500],
-    pink[100],
     purple[400],
     indigo[900],
     blue[300],
     teal[300],
-    green[400],
     lightGreen[900],
     lime[500],
     blueGrey[500]
 ];
+
+if (process.env.NODE_ENV === "development") {
+    if (planColors.length !== Math.pow(2, Math.floor(Math.log2(planColors.length)))) {
+        // eslint-disable-next-line no-console
+        console.error("PlanColors has %s entries, which isn't a power of two!!",
+            planColors.length);
+    }
+}
 
 const ensureInt = (id: number | string) => {
     if (typeof id == "number") {
@@ -31,4 +36,15 @@ const ensureInt = (id: number | string) => {
     return parseInt(id, 10);
 };
 
-export const colorHash = (id: number | string) => planColors[ensureInt(id) % planColors.length];
+export const colorHash = (id: number | string) => {
+    const l = planColors.length;
+    let n = ensureInt(id);
+    let h = 0;
+    // This XORs all the bits of the number, instead of just the last few, which
+    // depends on the modulus being a power of two for a fair distribution.
+    while (n > 0) {
+        h ^= n % l;
+        n = Math.floor(n / l);
+    }
+    return planColors[h];
+};
