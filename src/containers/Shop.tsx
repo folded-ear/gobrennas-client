0859297c1aa3df51c32e1a4ae02f7f26048cdebb
Barney Boisvert <bboisvert@gmail.com>
2023-07-28 18:23:22 -0700

    highlight ingredient breakdowns correctly as well

--- src/containers/Shop.tsx
+++ src/containers/Shop.tsx
@@ -163,6 +163,7 @@ function groupItems(plans: PlanItem[],
                 blockId: ingId,
                 ingredient,
                 ...it,
+                acquiring: isAcquiring(it),
             })));
         }
     }
