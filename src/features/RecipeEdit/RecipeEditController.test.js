569a4d46bdbdd93b8b0d08214270fa2f8e7ecbc2
bswitzer <brenna.switzer@gmail.com>
2024-03-23 13:45:10 -0700

    Make notes for myself for next time

--- src/features/RecipeEdit/RecipeEditController.test.js
+++ src/features/RecipeEdit/RecipeEditController.test.js
@@ -1,44 +1,13 @@
-import RecipeEditController from "./RecipeEditController";
-import { MockedProvider } from "@apollo/client/testing";
-import { renderWithProviders, screen } from "test/test-utils";
-import { MemoryRouter, Route } from "react-router-dom";
-import { ThemeProvider } from "@mui/material/styles";
-import theme from "../../theme";
-
-// Duplicate a recipe
-// delete recipe
-// share recipe
-// edit recipe (and all the trappings of it)
-// adding a new recipe
-// adding a new ingredient
-// save as copy
+// TODO: Duplicate a recipe
+// TODO: delete recipe
+// TODO: share recipe
+// TODO: edit recipe (and all the trappings of it)
+// TODO: adding a new recipe
+// TODO: adding a new ingredient
+// TODO: save as copy
 
 describe("Editing a Recipe", () => {
-    it("loads with selected recipe", async () => {
-        renderWithProviders(
-            <ThemeProvider theme={theme}>
-                <MemoryRouter initialEntries={["/library/recipe/1/edit"]}>
-                    <MockedProvider mocks={mocks} addTypename={false}>
-                        <Route path="/library/recipe/:id/edit">
-                            <RecipeEditController />
-                        </Route>
-                    </MockedProvider>
-                </MemoryRouter>
-            </ThemeProvider>,
-        );
-        const something = await screen.findByText("Honey Ham");
-        console.log(something);
-    });
-
-    it("changes text values correctly", () => {
-        expect(true).toBe(true);
-    });
-
-    it("saves a copy", () => {
-        expect(true).toBe(true);
-    });
-
-    it("redirects on save", () => {
+    it("fetches selected recipe", async () => {
         expect(true).toBe(true);
     });
 
