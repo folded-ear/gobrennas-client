8b77c17ab74e77a83916ff11407d92da798b4fcb
Barney Boisvert <bboisvert@gmail.com>
2024-04-06 19:16:34 -0700

    populate via unconditional server query

---
+++ src/data/hooks/usePantryItemSearch.ts
@@ -0,0 +1,91 @@
+import { gql } from "__generated__";
+import { useQuery } from "@apollo/client";
+import { UseQueryResult } from "data/types";
+import { BfsId } from "global/types/identity";
+import { useMemo } from "react";
+import {
+    InputMaybe,
+    PageInfo,
+    PantryItemConnectionEdge,
+    SortDir,
+} from "../../__generated__/graphql";
+
+// This is stupid, but its either increase the duplication even further or lose
+// type safety. I'd rather abuse the server/database. I mentioned it's stupid?
+const SEARCH_PANTRY_ITEMS_QUERY = gql(`
+query pantryItems($query: String!, $first: NonNegativeInt, $after: Cursor, $sortBy: String, $sortDir: SortDir) {
+  pantry {
+    search(query: $query, first: $first, after: $after, sortBy: $sortBy, sortDir: $sortDir) {
+      edges {
+        node {
+          id
+          name
+          storeOrder
+          synonyms
+          labels
+          firstUse
+          myUseCount: useCount(scope: MINE)
+          allUseCount: useCount(scope: EVERYONE)
+        }
+      }
+      pageInfo {
+        hasPreviousPage
+        hasNextPage
+        startCursor
+        endCursor
+      }
+    }
+  }
+}
+`);
+
+export type Result = Pick<
+    PantryItemConnectionEdge["node"],
+    "name" | "storeOrder" | "synonyms" | "labels"
+> & {
+    id: BfsId;
+    firstUse: Date;
+    myUseCount: PantryItemConnectionEdge["node"]["useCount"];
+    allUseCount: PantryItemConnectionEdge["node"]["useCount"];
+};
+
+interface Results {
+    results: Result[];
+    pageInfo: PageInfo;
+}
+
+export const usePantryItemSearch = (
+    query: string,
+    first = 25,
+    after: InputMaybe<string> = null,
+    sortBy = "name",
+    sortDir = SortDir.Asc,
+): UseQueryResult<Results> => {
+    const {
+        loading,
+        error,
+        data: rawData,
+    } = useQuery(SEARCH_PANTRY_ITEMS_QUERY, {
+        variables: { query, first, after, sortBy, sortDir },
+    });
+    const data = useMemo(() => {
+        if (!rawData?.pantry?.search) {
+            return null;
+        }
+        const { edges, pageInfo } = rawData.pantry.search;
+        return {
+            results: edges.map((e) => ({
+                ...e.node,
+                // why a DateTime scalar has to be manually parsed is unclear...
+                firstUse: new Date(e.node.firstUse),
+            })),
+            pageInfo,
+        };
+    }, [rawData]);
+
+    return {
+        loading,
+        error,
+        data,
+    };
+};
--- src/features/PantryItemAdmin/PantryItemAdmin.tsx
+++ src/features/PantryItemAdmin/PantryItemAdmin.tsx
@@ -1,83 +1,92 @@
 import { Box, Paper } from "@mui/material";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import {
     DataGrid,
     GridColDef,
+    GridColumnVisibilityModel,
     GridFilterModel,
     GridPaginationModel,
     GridRowSelectionModel,
     GridSortModel,
 } from "@mui/x-data-grid";
-import { humanStringComparator } from "../../util/comparators";
-import { PageInfo } from "../../__generated__/graphql";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import {
+    Result,
+    usePantryItemSearch,
+} from "../../data/hooks/usePantryItemSearch";
 
-interface Row {
-    id: number;
-    lastName: string;
-    firstName: string | null;
-    age: number | null;
-}
-
-const COLUMNS: GridColDef<Row[][number]>[] = [
+const formatStringSet = (value: string[]) => value.join(", ");
+const parseStringSet = (value: string) =>
+    value
+        .split(",")
+        .map((s) => s.trim())
+        .filter((s) => s);
+const COLUMNS: GridColDef<Result[][number]>[] = [
     { field: "id", headerName: "ID", flex: 0.5 },
     {
-        field: "firstName",
-        headerName: "First name",
+        field: "name",
+        headerName: "Name",
         flex: 1,
         editable: true,
     },
     {
-        field: "lastName",
-        headerName: "Last name",
-        flex: 1,
-        editable: true,
-    },
-    {
-        field: "age",
-        headerName: "Age",
-        type: "number",
-        flex: 0.5,
-        editable: true,
-    },
-    {
-        field: "fullName",
-        headerName: "Full name",
-        description: "This column has a value getter and is not sortable.",
+        field: "synonyms",
+        headerName: "Synonyms",
+        type: "string",
         flex: 2,
+        editable: true,
+        sortable: false,
+        valueFormatter: formatStringSet,
+        valueParser: parseStringSet,
+    },
+    {
+        field: "labels",
+        headerName: "Labels",
+        type: "string",
+        flex: 2,
+        editable: true,
+        sortable: false,
+        valueFormatter: formatStringSet,
+        valueParser: parseStringSet,
+    },
+    {
+        field: "storeOrder",
+        headerName: "Shop",
+        description: "Shopping and/or store order",
+        flex: 0.5,
+        editable: false,
+    },
+    {
+        field: "useCounts",
+        headerName: "Uses",
+        description: "Use count for this item: your / everyone's",
+        flex: 0.5,
+        editable: false,
         sortable: false,
         valueGetter: (_value, row) =>
-            `${row.firstName || ""} ${row.lastName || ""}`,
+            `${row.myUseCount || 0} / ${row.allUseCount || 0}`,
+    },
+    {
+        field: "firstUse",
+        headerName: "First Use",
+        type: "date",
+        flex: 1,
     },
 ];
 
-const ALL_ROWS: Row[] = [
-    { id: 1, lastName: "Snow", firstName: "Jon", age: 14 },
-    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 31 },
-    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 31 },
-    { id: 4, lastName: "Stark", firstName: "Arya", age: 11 },
-    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-];
-for (let i = 0; i < 5; i++) {
-    ALL_ROWS.push(
-        ...ALL_ROWS.map((r) => ({ ...r, id: r.id + ALL_ROWS.length })),
-    );
-}
-
-interface Query {
-    loading?: boolean;
-    data?: Row[];
-    pageInfo?: PageInfo;
-}
-
-const PAGE_SIZE = 25;
-
 export default function PantryItemAdmin() {
+    const { loading, error, data } = usePantryItemSearch("");
+    const { results, pageInfo } = data || {};
+    const query = { loading, data: results, pageInfo };
+
+    // todo: put colViz in a preference
+    const [columnVizModel, setColumnVizModel] =
+        useState<GridColumnVisibilityModel>({
+            id: false,
+            firstUse: false,
+            storeOrder: false,
+        });
     const [filterModel, setFilterModel] = useState<GridFilterModel>({
         items: [], // unused, but required
         quickFilterValues: [], // what we care about
@@ -85,41 +94,41 @@ export default function PantryItemAdmin() {
     const [sortModel, setSortModel] = useState<GridSortModel>([]);
     const [pageModel, setPageModel] = useState<GridPaginationModel>({
         page: 0,
-        pageSize: PAGE_SIZE,
+        pageSize: 25,
     });
     const [selectionModel, setSelectionModel] = useState<GridRowSelectionModel>(
         [],
     );
-    const [query, setQuery] = useState<Query>({});
     const [saving, setSaving] = useState(false);
-    useEffect(() => {
-        setQuery({ loading: true });
-        const timeout = setTimeout(() => {
-            const query = (filterModel.quickFilterValues || [])
-                .join(" ")
-                .toLowerCase();
-            const matches = ALL_ROWS.filter((r) =>
-                JSON.stringify(r).toLowerCase().includes(query),
-            );
-            if (sortModel.length > 0) {
-                const f = sortModel[0].field;
-                const comp =
-                    sortModel[0].sort === "desc"
-                        ? (a, b) => humanStringComparator(b, a)
-                        : humanStringComparator;
-                if (f) {
-                    matches.sort((a, b) => comp(a[f], b[f]));
-                }
-            }
-            const { page: p, pageSize: ps } = pageModel;
-            const offset = p * ps;
-            setQuery({
-                loading: false,
-                data: matches.slice(offset, offset + ps),
-            });
-        }, 250);
-        return () => clearTimeout(timeout);
-    }, [filterModel, sortModel, pageModel]);
+    // const [query, setQuery] = useState<Query>({});
+    // useEffect(() => {
+    //     setQuery({ loading: true });
+    //     const timeout = setTimeout(() => {
+    //         const query = (filterModel.quickFilterValues || [])
+    //             .join(" ")
+    //             .toLowerCase();
+    //         const matches = ALL_ROWS.filter((r) =>
+    //             JSON.stringify(r).toLowerCase().includes(query),
+    //         );
+    //         if (sortModel.length > 0) {
+    //             const f = sortModel[0].field;
+    //             const comp =
+    //                 sortModel[0].sort === "desc"
+    //                     ? (a, b) => humanStringComparator(b, a)
+    //                     : humanStringComparator;
+    //             if (f) {
+    //                 matches.sort((a, b) => comp(a[f], b[f]));
+    //             }
+    //         }
+    //         const { page: p, pageSize: ps } = pageModel;
+    //         const offset = p * ps;
+    //         setQuery({
+    //             loading: false,
+    //             data: matches.slice(offset, offset + ps),
+    //         });
+    //     }, 250);
+    //     return () => clearTimeout(timeout);
+    // }, [filterModel, sortModel, pageModel]);
 
     function toFirstPage() {
         setPageModel((m) => (m.page === 0 ? m : { ...m, page: 0 }));
@@ -154,9 +163,9 @@ You sure?`,
         );
     }
 
-    function handleRowUpdate(newRow: Row, oldRow: Row) {
+    function handleRowUpdate(newRow: Result, oldRow: Result) {
         setSaving(true);
-        return new Promise<Row>((resolve, reject) => {
+        return new Promise<Result>((resolve, reject) => {
             setTimeout(() => {
                 if (newRow.id === 15) {
                     reject("Not 15, yo!");
@@ -187,6 +196,8 @@ You sure?`,
                 disableColumnMenu
                 disableColumnSelector={false}
                 density={"compact"}
+                columnVisibilityModel={columnVizModel}
+                onColumnVisibilityModelChange={setColumnVizModel}
                 disableRowSelectionOnClick
                 checkboxSelection
                 rowSelectionModel={selectionModel}
