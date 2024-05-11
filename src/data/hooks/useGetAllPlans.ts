bcd12fc3798e5a40d7f8dda0845be42e7b00dfc2
bswitzer <brenna.switzer@gmail.com>
2024-05-11 12:41:47 -0700

    adds sort to hook results

--- src/data/hooks/useGetAllPlans.ts
+++ src/data/hooks/useGetAllPlans.ts
@@ -2,6 +2,9 @@ import { gql } from "__generated__";
 import useAdaptingQuery from "data/hooks/useAdaptingQuery";
 import { GetPlansQuery } from "__generated__/graphql";
 import { Plan } from "features/Planner/types";
+import { useProfileId } from "providers/Profile";
+import { BfsId } from "global/types/identity";
+import { zippedComparator } from "util/comparators";
 
 export const GET_PLANS = gql(`
 query getPlans {
@@ -18,13 +21,38 @@ query getPlans {
 }
 `);
 
+const orderComponentsById = (plans: Plan[], userId: BfsId) => {
+    return plans.reduce((byId, plan) => {
+        let ownerId = plan.owner.id || Number.MAX_SAFE_INTEGER;
+        const ownerName = plan.owner.name || "";
+
+        if (ownerId === userId) {
+            ownerId = -1;
+        }
+        byId[plan.id] = [ownerId, ownerName, plan.name];
+        return byId;
+    }, {});
+};
+
 const adapter = (data?: GetPlansQuery): Plan[] => {
     if (!data?.planner?.plans) return [];
     return data.planner.plans as Plan[];
 };
 
 export const useGetAllPlans = () => {
-    return useAdaptingQuery(GET_PLANS, adapter, {
+    const userId = useProfileId();
+    const result = useAdaptingQuery(GET_PLANS, adapter, {
         fetchPolicy: "network-only",
     });
+    const orderedPlans = orderComponentsById(result.data, userId);
+    const plans = result.data
+        .slice()
+        .sort((a, b) =>
+            zippedComparator(orderedPlans[a.id], orderedPlans[b.id]),
+        ) as Plan[];
+
+    return {
+        ...result,
+        data: plans,
+    };
 };
