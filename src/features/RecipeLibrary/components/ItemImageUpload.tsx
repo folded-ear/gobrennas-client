de658c5f2973b38171733c9716110b743c2de799
Barney Boisvert <bboisvert@gmail.com>
2024-05-12 09:34:30 -0700

    set recipe photos via graphql, so the cache updates

--- src/features/RecipeLibrary/components/ItemImageUpload.tsx
+++ src/features/RecipeLibrary/components/ItemImageUpload.tsx
@@ -1,9 +1,23 @@
 import { makeStyles } from "@mui/styles";
-import Dispatcher from "data/dispatcher";
-import RecipeActions from "data/RecipeActions";
 import React from "react";
 import ImageDropZone from "util/ImageDropZone";
 import { BfsId } from "global/types/identity";
+import { gql } from "../../../__generated__";
+import { useMutation } from "@apollo/client";
+import promiseWellSizedFile from "../../../util/promiseWellSizedFile";
+
+const SET_RECIPE_PHOTO = gql(`
+mutation setRecipePhoto($id: ID!, $photo: Upload!) {
+  library {
+    setRecipePhoto(id: $id, photo: $photo) {
+      id
+      photo {
+        url
+        focus
+      }
+    }
+  }
+}`);
 
 const useStyles = makeStyles({
     root: {
@@ -22,17 +36,24 @@ interface Props {
 
 const ItemImageUpload: React.FC<Props> = ({ recipeId, disabled }) => {
     const classes = useStyles();
+    const [setRecipePhoto] = useMutation(SET_RECIPE_PHOTO);
+    const handlePhoto = async (photo) => {
+        if (!(photo instanceof File)) {
+            throw new Error("Non-File photo? Huh?");
+        }
+        photo = await promiseWellSizedFile(photo);
+        setRecipePhoto({
+            variables: {
+                id: "" + recipeId,
+                photo,
+            },
+        });
+    };
     return (
         <ImageDropZone
             disabled={disabled}
             className={classes.root}
-            onImage={(file) =>
-                Dispatcher.dispatch({
-                    type: RecipeActions.SET_RECIPE_PHOTO,
-                    id: recipeId,
-                    photo: file,
-                })
-            }
+            onImage={handlePhoto}
         />
     );
 };
