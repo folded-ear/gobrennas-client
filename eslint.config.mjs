f57d4c8a5556427161eb1ce947c8d9bc26d46043
bswitzer <brenna.switzer@gmail.com>
2024-08-04 11:07:52 -0700

    adapt configuration for vite

---
+++ eslint.config.mjs
@@ -0,0 +1,43 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReactConfig,
+  {
+    ignores: ["**/__generated__/**/*"]
+  },
+  {
+    rules: {
+      "no-console": 1,
+      "no-debugger": "warn",
+      "no-prototype-builtins": 0,
+      "no-restricted-imports": [
+        "error",
+        {
+          "paths": ["@mui/icons-material"],
+          "patterns": ["@mui/icons-material/*"]
+        }
+      ],
+      "quotes": [
+        "warn",
+        "double",
+        {
+          "avoidEscape": true,
+          "allowTemplateLiterals": true
+        }
+      ],
+      "react/prop-types": 1,
+      "semi": ["warn", "always"],
+      "@typescript-eslint/no-explicit-any": 0,
+      "@typescript-eslint/no-unsafe-assignment": 0
+    }
+  }
+];
---
+++ vite.config.ts
@@ -0,0 +1,128 @@
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+    server: {
+        port: 3001,
+    },
+    build: {
+        outDir: "build",
+    },
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: "prompt",
+            injectRegister: false,
+            pwaAssets: {
+                disabled: false,
+                config: true,
+            },
+            manifest: {
+                short_name: "Food Software",
+                name: "Brenna's Food Software",
+                icons: [
+                    {
+                        src: "android-chrome-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                    {
+                        src: "android-chrome-384x384.png",
+                        sizes: "384x384",
+                        type: "image/png",
+                    },
+                    {
+                        src: "android-chrome-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "android-chrome-96x96.png",
+                        sizes: "96x96",
+                        type: "image/png",
+                    },
+                ],
+                scope: "/",
+                start_url: "/shop",
+                display: "standalone",
+                theme_color: "#F57F17",
+                background_color: "#ffffff",
+                shortcuts: [
+                    {
+                        name: "Shop",
+                        url: "/shop",
+                        icons: [
+                            {
+                                src: "shop-96x96.png",
+                                sizes: "96x96",
+                            },
+                            {
+                                src: "shop-192x192.png",
+                                sizes: "192x192",
+                            },
+                        ],
+                    },
+                    {
+                        name: "Plan",
+                        url: "/plan",
+                        icons: [
+                            {
+                                src: "plan-96x96.png",
+                                sizes: "96x96",
+                            },
+                            {
+                                src: "plan-192x192.png",
+                                sizes: "192x192",
+                            },
+                        ],
+                    },
+                    {
+                        short_name: "Recipes",
+                        name: "Recipe Library",
+                        url: "/library",
+                        icons: [
+                            {
+                                src: "library-96x96.png",
+                                sizes: "96x96",
+                            },
+                            {
+                                src: "library-192x192.png",
+                                sizes: "192x192",
+                            },
+                        ],
+                    },
+                ],
+            },
+            workbox: {
+                globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+                cleanupOutdatedCaches: true,
+                clientsClaim: true,
+            },
+            devOptions: {
+                enabled: false,
+                navigateFallback: "index.html",
+                suppressWarnings: true,
+                type: "module",
+            },
+        }),
+    ],
+    test: {
+        globals: true,
+        environment: "jsdom",
+        css: true,
+        reporters: ["verbose"],
+        coverage: {
+            reporter: ["text", "json", "html"],
+            include: ["src/**/*"],
+            exclude: [],
+        },
+    },
+});
