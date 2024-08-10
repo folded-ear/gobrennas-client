5aae157b7732e793f4c426479d10709791a74da0
Barney Boisvert <bboisvert@gmail.com>
2024-08-10 13:11:28 -0700

    use an @-imports, instead of ../s

--- src/features/Planner/components/ResetBucketsButton.tsx
+++ src/features/Planner/components/ResetBucketsButton.tsx
@@ -1,10 +1,10 @@
 import IconButton from "@mui/material/IconButton";
-import dispatcher from "../../../data/dispatcher";
+import dispatcher from "@/data/dispatcher";
 import PlanActions from "../data/PlanActions";
-import { GenerateBucketsIcon } from "../../../views/common/icons";
+import { GenerateBucketsIcon } from "@/views/common/icons";
 import Tooltip from "@mui/material/Tooltip";
 import React from "react";
-import { BfsId } from "../../../global/types/identity";
+import { BfsId } from "@/global/types/identity";
 import { IconButtonProps } from "@mui/material";
 
 const ResetBucketsButton: React.FC<{ planId: BfsId } & IconButtonProps> = ({
