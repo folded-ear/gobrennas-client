8ea70f089120f9357ace1f61fdf908538e8bece6
bswitzer <brenna.switzer@gmail.com>
2024-09-02 16:48:53 -0700

    add devMode feature flag

--- src/features/Planner/components/LibrarySearchSidebar.tsx
+++ src/features/Planner/components/LibrarySearchSidebar.tsx
@@ -1,10 +1,12 @@
 import { SidebarDrawer } from "@/global/components/Sidebar";
 import { LibrarySearchController } from "@/features/LibrarySearch/LibrarySearchController";
+import useIsDevMode from "@/data/useIsDevMode";
 
 export const LibrarySearchSidebar = () => {
-    return (
+    const devMode = useIsDevMode();
+    return devMode ? (
         <SidebarDrawer>
             <LibrarySearchController />
         </SidebarDrawer>
-    );
+    ) : null;
 };
