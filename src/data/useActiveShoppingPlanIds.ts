4256b20756f4a965520b85fe24cae756c921e41a
Barney Boisvert <bboisvert@gmail.com>
2024-09-29 15:19:01 -0700

    move redirecting after plan delete into the react tree

--- src/data/useActiveShoppingPlanIds.ts
+++ src/data/useActiveShoppingPlanIds.ts
@@ -6,7 +6,9 @@ function useActiveShoppingPlanIds() {
     return useFluxStore(() => {
         const ids = shoppingStore.getActivePlanIds();
         if (ids != null && ids.length > 0) return ids;
-        return [planStore.getActivePlanRlo().data?.id];
+        const activePlan = planStore.getActivePlanRlo().data;
+        if (activePlan && activePlan.id != null) return [activePlan?.id];
+        return [];
     }, [planStore, shoppingStore]);
 }
 
--- src/features/RecipeDisplay/hooks/useLoadedPlan.ts
+++ src/features/RecipeDisplay/hooks/useLoadedPlan.ts
@@ -5,8 +5,10 @@ import Dispatcher from "@/data/dispatcher";
 import PlanActions from "@/features/Planner/data/PlanActions";
 import { BfsId } from "@/global/types/identity";
 import { ensureInt } from "@/global/utils";
+import { useHistory } from "react-router-dom";
 
 export const useLoadedPlan = (pid: BfsId | undefined) => {
+    const history = useHistory();
     // ensure it's loaded
     const allPlansRlo = useFluxStore(
         () => planStore.getPlanIdsRlo(),
@@ -14,17 +16,23 @@ export const useLoadedPlan = (pid: BfsId | undefined) => {
     );
     // ensure it's selected
     React.useEffect(() => {
-        if (pid != null && allPlansRlo?.data) {
-            // The contract implies that effects trigger after the render
-            // cycle completes, but doesn't guarantee it. The setTimeout
-            // avoids a reentrant dispatch if the effect isn't deferred.
-            const t = setTimeout(() =>
-                Dispatcher.dispatch({
-                    type: PlanActions.SELECT_PLAN,
-                    id: ensureInt(pid),
-                }),
-            );
-            return () => clearTimeout(t);
+        if (pid != null && allPlansRlo?.data && allPlansRlo.data.length) {
+            const id = ensureInt(pid);
+            if (!allPlansRlo.data.includes(id)) {
+                // Deleted; navigate to the first still-present plan.
+                history.push(`/plan/${allPlansRlo.data[0]}`);
+            } else {
+                // The contract implies that effects trigger after the render
+                // cycle completes, but doesn't guarantee it. The setTimeout
+                // avoids a reentrant dispatch if the effect isn't deferred.
+                const t = setTimeout(() =>
+                    Dispatcher.dispatch({
+                        type: PlanActions.SELECT_PLAN,
+                        id,
+                    }),
+                );
+                return () => clearTimeout(t);
+            }
         }
-    }, [allPlansRlo?.data, pid]);
+    }, [history, allPlansRlo?.data, pid]);
 };
