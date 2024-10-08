9b9586741ad0faa2f82abc2730644915de08abc8
Barney Boisvert <bboisvert@gmail.com>
2024-10-08 08:35:50 -0700

    bfsIdEq and ensureString helpers for BfsId

---
+++ src/global/types/identity.test.ts
@@ -0,0 +1,76 @@
+import { describe, expect, it } from "vitest";
+import { BfsId, bfsIdEq, ensureString } from "./identity";
+
+describe("identity", () => {
+    const tooBig = 9007199254740992;
+    describe("ensureString", () => {
+        it("should refuse negatives", () => {
+            expect(() => ensureString(-123)).toThrow("negative");
+            expect(ensureString("-123")).toBe("-123");
+        });
+        it("should refuse non-integers", () => {
+            expect(() => ensureString(1.23)).toThrow("integer");
+            expect(ensureString("1.23")).toBe("1.23");
+        });
+        it("should refuse imprecise (too-large) values", () => {
+            expect(() => ensureString(tooBig)).toThrow("large");
+            expect(ensureString("9007199254740992")).toBe("9007199254740992");
+        });
+    });
+    describe("bfsIdEq", () => {
+        it("should equate numbers", () => {
+            expect(bfsIdEq(123, 123)).toBe(true);
+            expect(bfsIdEq(123, 456)).toBe(false);
+        });
+        it("should equate strings", () => {
+            expect(bfsIdEq("abc", "abc")).toBe(true);
+            expect(bfsIdEq("abc", "def")).toBe(false);
+            expect(bfsIdEq("abc", "ABC")).toBe(false);
+            expect(
+                bfsIdEq("123456789098765432123456", "123456789098765432123456"),
+            ).toBe(true);
+        });
+        it("should coerce to string", () => {
+            expect(bfsIdEq("123", 123)).toBe(true);
+            expect(bfsIdEq(123, "123")).toBe(true);
+            expect(bfsIdEq(123, "abc")).toBe(false);
+            // the largest allowed
+            expect(bfsIdEq(9007199254740991, "9007199254740991")).toBe(true);
+        });
+        it("should balk on negative numeric ids", () => {
+            expect(() => bfsIdEq(-123, "-123")).toThrow();
+            expect(() => bfsIdEq(-123, -123)).toThrow();
+            expect(() => bfsIdEq("-123", -123)).toThrow();
+            expect(bfsIdEq("-123", "-123")).toBe(true);
+        });
+        it("should balk on non-integer numeric ids", () => {
+            expect(() => bfsIdEq("12.3", 12.3)).toThrow();
+            expect(() => bfsIdEq(12.3, "12.3")).toThrow();
+            expect(() => bfsIdEq(12.3, 12.3)).toThrow();
+            expect(bfsIdEq("12.3", "12.3")).toBe(true);
+        });
+        it("should balk on imprecise (too-large) numeric ids", () => {
+            expect(() => bfsIdEq(tooBig, "9007199254740992")).toThrow();
+            expect(() => bfsIdEq("9007199254740992", tooBig)).toThrow();
+            expect(() => bfsIdEq(tooBig, tooBig)).toThrow();
+            expect(bfsIdEq("9007199254740992", "9007199254740992")).toBe(true);
+        });
+        it("should equate null, even though TS won't let you pass one", () => {
+            expect(
+                bfsIdEq(null as unknown as BfsId, null as unknown as BfsId),
+            ).toBe(true);
+            expect(bfsIdEq(null as unknown as BfsId, "abc")).toBe(false);
+            expect(bfsIdEq(123, null as unknown as BfsId)).toBe(false);
+        });
+        it("should equate undefined, even though TS won't let you pass one", () => {
+            expect(
+                bfsIdEq(
+                    undefined as unknown as BfsId,
+                    undefined as unknown as BfsId,
+                ),
+            ).toBe(true);
+            expect(bfsIdEq(undefined as unknown as BfsId, "abc")).toBe(false);
+            expect(bfsIdEq(123, undefined as unknown as BfsId)).toBe(false);
+        });
+    });
+});
--- src/global/types/identity.ts
+++ src/global/types/identity.ts
@@ -6,3 +6,22 @@ export type UserType = Pick<
     User,
     "id" | "name" | "provider" | "email" | "imageUrl" | "roles"
 >;
+
+export function ensureString(id: BfsId): string {
+    if (typeof id === "string") return id;
+    if (id < 0) throw new TypeError("Ids must be non-negative.");
+    if (id > Number.MAX_SAFE_INTEGER)
+        throw new TypeError(
+            "Ids must not be so large they lose precision to floating point representation.",
+        );
+    if (id != Math.floor(id)) throw new TypeError("Ids must be integers");
+    return id.toString();
+}
+
+export function bfsIdEq(a: BfsId, b: BfsId): boolean {
+    // soak up null/undefined that get passed via any (e.g., from JS)
+    if (a == null) return b == null;
+    if (b == null) return false;
+    // stringify and test!
+    return ensureString(a) === ensureString(b);
+}
