b42ebc9993230401a0ef78955f874bc4ace91ac7
Barney Boisvert <bboisvert@gmail.com>
2025-01-26 18:55:04 -0800

    typescriptify RecipeApi

--- src/data/RecipeApi.ts
+++ src/data/RecipeApi.ts
@@ -3,13 +3,15 @@ import queryClient from "@/data/queryClient";
 import promiseFlux from "@/util/promiseFlux";
 import { API_BASE_URL } from "@/constants";
 import RecipeActions from "./RecipeActions";
+import { BfsId } from "@/global/types/identity";
+import { Maybe } from "graphql/jsutils/Maybe";
 
 const axios = BaseAxios.create({
     baseURL: `${API_BASE_URL}/api`,
 });
 
 const RecipeApi = {
-    sendToPlan(recipeId, planId, scale) {
+    sendToPlan(recipeId: BfsId, planId: BfsId, scale: Maybe<number>) {
         promiseFlux(
             axios.post(
                 `/recipe/${recipeId}/_send_to_plan/${planId}?scale=${
