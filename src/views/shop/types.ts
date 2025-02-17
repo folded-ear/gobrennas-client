a2c8ce2bb28d5e21bbdb76cfcd64470324cdc249
Barney Boisvert <bboisvert@gmail.com>
2025-02-16 16:10:30 -0800

    ... shopping

--- src/views/shop/types.ts
+++ src/views/shop/types.ts
@@ -1,14 +1,19 @@
+import PlanItemStatus from "@/features/Planner/data/PlanItemStatus";
 import type { BfsId } from "@/global/types/identity";
 
 export type TupleProps = {
     active?: boolean;
-    classes: any;
+    // CSS classes from withItemStyles
+    classes: Record<
+        "acquiring" | "active" | "deleting" | "question" | "text",
+        string
+    >;
 };
 
 export type BaseItemProp = {
     id: BfsId;
     name: string;
-    status?: string;
+    status?: PlanItemStatus;
 };
 
 export type ItemProps = BaseItemProp & {
