356aa772362e3721bee9dcb3043bdda91bbdb4c8
Barney Boisvert <bboisvert@gmail.com>
2025-08-02 12:38:27 -0700

    use state updaters to avoid data loss from batching

--- src/data/hooks/useRecipeForm.ts
+++ src/data/hooks/useRecipeForm.ts
@@ -46,24 +46,29 @@ export function useRecipeForm(recipe: Recipe<unknown>): UseRecipeFormReturn {
     };
 
     const onAddIngredientRef = (idx?: number) => {
-        const ing = buildNewIngredient();
-        const ings =
-            draft.ingredients == null ? [] : draft.ingredients.slice(0);
-        const newIdx = idx ?? ings.length;
-        if (newIdx < 0 || newIdx >= ings.length) {
-            ings.push(ing);
-        } else {
-            ings.splice(newIdx + 1, 0, ing);
-        }
-        setDraft(dotProp.set(draft, "ingredients", ings));
+        setDraft((draft) => {
+            const ing = buildNewIngredient();
+            const ings =
+                draft.ingredients == null ? [] : draft.ingredients.slice(0);
+            const newIdx = idx ?? ings.length;
+            if (newIdx < 0 || newIdx >= ings.length) {
+                ings.push(ing);
+            } else {
+                ings.splice(newIdx + 1, 0, ing);
+            }
+            return dotProp.set(draft, "ingredients", ings);
+        });
     };
 
     const onDeleteIngredientRef = (idx: number) => {
-        if (draft.ingredients && idx <= draft.ingredients.length) {
-            const ings = draft.ingredients.slice(0);
-            ings.splice(idx, 1);
-            setDraft(dotProp.set(draft, "ingredients", ings));
-        }
+        setDraft((draft) => {
+            if (draft.ingredients && idx <= draft.ingredients.length) {
+                const ings = draft.ingredients.slice(0);
+                ings.splice(idx, 1);
+                draft = dotProp.set(draft, "ingredients", ings);
+            }
+            return draft;
+        });
     };
 
     const onMoveIngredientRef = (
@@ -71,37 +76,46 @@ export function useRecipeForm(recipe: Recipe<unknown>): UseRecipeFormReturn {
         targetId: BfsId,
         above: boolean,
     ) => {
-        const ings =
-            draft.ingredients == null ? [] : draft.ingredients.slice(0);
-        const idxActive = ings.findIndex((it) => it.id === activeId);
-        if (idxActive > 0) {
-            const removed = ings.splice(idxActive, 1);
-            const idxTarget = ings.findIndex((it) => it.id === targetId);
-            if (idxTarget > 0) {
-                ings.splice(above ? idxTarget : idxTarget + 1, 0, ...removed);
+        setDraft((draft) => {
+            const ings =
+                draft.ingredients == null ? [] : draft.ingredients.slice(0);
+            const idxActive = ings.findIndex((it) => it.id === activeId);
+            if (idxActive > 0) {
+                const removed = ings.splice(idxActive, 1);
+                const idxTarget = ings.findIndex((it) => it.id === targetId);
+                if (idxTarget > 0) {
+                    ings.splice(
+                        above ? idxTarget : idxTarget + 1,
+                        0,
+                        ...removed,
+                    );
+                }
             }
-        }
-        setDraft(dotProp.set(draft, "ingredients", ings));
+            return dotProp.set(draft, "ingredients", ings);
+        });
     };
 
     const onMultilinePasteIngredientRefs = (idx: number, text: string) => {
-        const ings =
-            draft.ingredients == null ? [] : draft.ingredients.slice(0);
-        const newIndx = idx < 0 ? 0 : idx < ings.length ? idx : ings.length - 1;
-        if (ings[newIndx].raw?.length === 0) {
-            // if pasting into an empty on, delete it
-            ings.splice(idx--, 1);
-        }
-        ings.splice(
-            ++idx,
-            0,
-            ...text
-                .split("\n")
-                .map((it) => it.trim())
-                .filter((it) => it.length > 0)
-                .map(buildNewIngredient),
-        );
-        setDraft(dotProp.set(draft, "ingredients", ings));
+        setDraft((draft) => {
+            const ings =
+                draft.ingredients == null ? [] : draft.ingredients.slice(0);
+            const newIndx =
+                idx < 0 ? 0 : idx < ings.length ? idx : ings.length - 1;
+            if (ings[newIndx].raw?.length === 0) {
+                // if pasting into an empty one, delete it
+                ings.splice(idx--, 1);
+            }
+            ings.splice(
+                ++idx,
+                0,
+                ...text
+                    .split("\n")
+                    .map((it) => it.trim())
+                    .filter((it) => it.length > 0)
+                    .map(buildNewIngredient),
+            );
+            return dotProp.set(draft, "ingredients", ings);
+        });
     };
 
     return {
